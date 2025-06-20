9ec6d1da9196395a96174216c6372b786e05dd6b
rickey johnson <rdjwinter2004@gmail.com>
2025-06-20 10:54:51 -0700

    added dark mode

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,10 +4,23 @@ import BoardDetails from './components/BoardDeatils'
 import { Header } from './components/Header'
 import { Footer } from './components/Footer'
 import './App.css'
+import { useEffect, useState } from 'react'
 
 const App = () => {
+	const [darkMode, setDarkMode] = useState(false)
+
+	useEffect(() => {
+		const root = document.documentElement
+		if (darkMode) {
+			root.classList.add('dark')
+		} else {
+			root.classList.remove('dark')
+		}
+	}, [darkMode])
+
 	return (
 		<div className="App">
+			<button className='dark-light-toggle' onClick={() => setDarkMode(!darkMode)}>Toggle {darkMode ? 'Light' : 'Dark'}</button>
 			<Header />
 			<Routes>
 				<Route path="/" element={<Home />} />
